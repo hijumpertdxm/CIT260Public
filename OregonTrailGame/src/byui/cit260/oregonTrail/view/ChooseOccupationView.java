7083755b32be421ce09b5b3391d9e757d53971c5
Edward Wilson <wil16098@byui.edu>
2018-03-07 19:19:41 -0600

    Working with Mason.

---
+++ OregonTrailGame/src/byui/cit260/oregonTrail/view/ChooseOccupationView.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.oregonTrail.view;
+
+/**
+ *
+ * @author SysAdmin
+ */
+public class ChooseOccupationView {
+    
+//    Display three Occupation Options
+//            The user chooses an option
+//                    the option is saved and goes to control
+//                            maybe goes to an instance of the model class?
+}
