b04258bbea2bedda446edcb1a5e4f88698fbc0fb
Edward Wilson <wil16098@byui.edu>
2018-03-07 19:26:02 -0600

    Working with Mason.

--- OregonTrailGame/src/byui/cit260/oregonTrail/view/HelpMenuView.java
+++ OregonTrailGame/src/byui/cit260/oregonTrail/view/HelpMenuView.java
@@ -11,52 +11,52 @@ import java.util.Scanner;
  *
  * @author Roller
  */
-public class HelpMenuView {
+public class HelpMenuView extends View {
 
-    public void displayHelpMenuView() {
-        boolean endView = false;
-        do{
-            String[] inputs = this.getInputs();
-            if((inputs.length < 1) || (inputs[0].toUpperCase().equals("Q"))){
-                return;
-            }
-            endView = doAction(inputs);
-        }while(endView != true);
-    }
-    
-    private String[] getInputs(){
-        
-        String[] inputs = new String[1];
-        
-        System.out.println("=====================================================");
-        System.out.println("Help Menu");
-        System.out.println("=====================================================");
-        System.out.println("G - What is the goal of the game?");
-        System.out.println("M - How to move");
-        System.out.println("R - Getting resources");
-        System.out.println("Q - Quit");
-        System.out.println("=====================================================");
-        
-        boolean valid = false;
-        while(valid == false){
-            System.out.println("Please enter the action to take below:");
-            Scanner scanner = new Scanner(System.in);
-            
-            //user string input
-            String userInput = scanner.nextLine().trim();
-            
-            if(userInput.length() < 1){
-                System.out.println("You must enter a non-blank value");
-                continue;
-            }
-            
-            inputs[0] = userInput;
-            valid = true;
-        }
-        
-        return inputs;
-    }
-    
+//    public void displayHelpMenuView() {
+//        boolean endView = false;
+//        do{
+//            String[] inputs = this.getInputs();
+//            if((inputs.length < 1) || (inputs[0].toUpperCase().equals("Q"))){
+//                return;
+//            }
+//            endView = doAction(inputs);
+//        }while(endView != true);
+//    }
+//    
+//    private String[] getInputs(){
+//        
+//        String[] inputs = new String[1];
+//        
+//        System.out.println("=====================================================");
+//        System.out.println("Help Menu");
+//        System.out.println("=====================================================");
+//        System.out.println("G - What is the goal of the game?");
+//        System.out.println("M - How to move");
+//        System.out.println("R - Getting resources");
+//        System.out.println("Q - Quit");
+//        System.out.println("=====================================================");
+//        
+//        boolean valid = false;
+//        while(valid == false){
+//            System.out.println("Please enter the action to take below:");
+//            Scanner scanner = new Scanner(System.in);
+//            
+//            //user string input
+//            String userInput = scanner.nextLine().trim();
+//            
+//            if(userInput.length() < 1){
+//                System.out.println("You must enter a non-blank value");
+//                continue;
+//            }
+//            
+//            inputs[0] = userInput;
+//            valid = true;
+//        }
+//        
+//        return inputs;
+//    }
+//    
     private boolean doAction(String[] inputs){
         
         //Set as first element and uppercase it
