1b0acb960b96c048b9e2dfc93576e3b96398b7e8
ayami <ayami@192.168.1.72>
2018-03-10 23:33:04 -0600

    winning and losing view

---
+++ OregonTrailGame/src/byui/cit260/oregonTrail/view/LosingView.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.oregonTrail.view;
+
+/**
+ *
+ * @author ayami
+ */
+public class LosingView extends View{
+    
+    public LosingView() {
+    
+        super (
+        "\n====================================================="
+        +"\n| *************** The Organ Trail  ***************" 
+        +"\n======================================================"
+        +"\n  ***********All wagon members have died***********"
+        +"\n"
+        +"\nfrom Independence, Missouri to Oregon"
+        +"\nFinal Results:"
+        +"\nRemaining places in the map:     XX"        
+        +"\nRemaining wagon members:         XX"
+        +"\nTotal remaining possesions:      XX"
+        +"\nTotal remaining cash:            XX"
+        +"\n"
+        +"\n N. Start a New Game"
+        +"\n E. Exit the game"        
+        +"\n ---- What is your choice?                            "           
+        );
+}
+//Second part
+@Override
+    public boolean doAction(String value){  
+        //Set as first element and uppercase it
+        value = value.toUpperCase();
+        
+        switch(value){
+            case "N":
+                this.newGame();
+                break;
+            case "E":
+                this.exitGame();
+                break;
+                
+            default:
+              System.out.println("Invalid menu item");
+        }   
+        return false;
+        }    
+
+    //Third part
+private void newGame() {
+        StartProgramView startProgramView = new StartProgramView();
+        startProgramView.display();
+    }
+
+    private void exitGame() {
+        //Exit 
+        System.exit(0);
+    }
+    
+}  
